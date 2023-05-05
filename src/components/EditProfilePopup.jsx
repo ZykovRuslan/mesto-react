5c7fe316997802a73dec0911e6fb7c89f0c74b09
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-05-05 11:29:17 +0400

    refactor

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -13,7 +13,7 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
   React.useEffect(() => {
     setName(currentUser.name);
     setDescription(currentUser.about);
-  }, [currentUser]);
+  }, [currentUser, isOpen]);
 
   function handleSubmit(evt) {
     // Запрещаем браузеру переходить по адресу формы
